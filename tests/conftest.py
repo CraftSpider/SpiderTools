f01b68a6480db985853ba6ab0b2304ff1a4e2990
Rune Tynan <runetynan@gmail.com>
2020-04-07 18:49:56 -0400

    Back to stdout, try showing skips

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,10 +57,11 @@ class TestBot(bot.ExtendedBot):
 @pytest.fixture(scope="session", autouse=True)
 def setup_logging():
     import logging
+    import sys
     log = logging.getLogger("spidertools")
     log.setLevel(logging.DEBUG)
     format = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
-    handle = logging.StreamHandler()
+    handle = logging.StreamHandler(stream=sys.stdout)
     handle.setFormatter(format)
     log.addHandler(handle)
 
